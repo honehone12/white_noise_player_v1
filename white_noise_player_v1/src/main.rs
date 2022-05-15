8d44dba0d4010e4392c66a6a0c08957cf6b5a4a0
honehone12 <marshalc@hotmail.co.jp>
2022-05-15 14:42:24 +0900

    initial commit

---
+++ white_noise_player_v1/src/main.rs
@@ -0,0 +1,131 @@
+use std::{ thread, time, io };
+use kira::manager::{ AudioManager, AudioManagerSettings };
+use kira::manager::backend::cpal::CpalBackend;
+use kira::sound::static_sound::{ StaticSoundData, StaticSoundSettings };
+use kira::{ tween, StartTime, Volume, LoopBehavior };
+use casual_logger::Log;
+
+fn main() {
+    Log::remove_old_logs();
+
+    let result = AudioManager::<CpalBackend>::new(
+            AudioManagerSettings::default()
+    );
+    let mut manager = match result {
+        Ok(m) => m,
+        Err(e) => {
+            panic!("{}", Log::fatal(&e.to_string()));
+        }
+    };
+    let result = StaticSoundData::from_file(
+        "noise.wav",
+        StaticSoundSettings::new().loop_behavior(
+            LoopBehavior {
+                start_position: 0.0
+            }
+        )
+    );
+    let sound_data = match result {
+        Ok(d) => d,
+        Err(e) => {
+            panic!(
+                "{}\nYou need to put noise.wav in the same directory.",
+                Log::fatal(&e.to_string())
+            );
+        } 
+    };
+    let result = manager.play(sound_data);
+    let mut handle = match result {
+        Ok(h) => h,
+        Err(e) => {
+            panic!("{}", Log::fatal(&e.to_string()));
+        }
+    };
+
+    let stdin = io::stdin();
+
+    println!("Welcome!!\nThis is WhiteNoisePlayer.\n");
+    println!("Please type...");
+    println!("[q] for quitting.");
+    println!("[w] for volume up.");
+    println!("[s] for volume down.");
+
+    let tick_delta = time::Duration::from_millis(500);
+    let mut current_volume = 0.0;
+    let volume_delta = 2.0;
+    let volume_tween = tween::Tween {
+        start_time: StartTime::Immediate,
+        duration: time::Duration::from_millis(250),
+        easing: tween::Easing::Linear
+    };
+    
+    loop
+    {
+        let mut buffer = String::new();
+        let result = stdin.read_line(&mut buffer);
+        match result {
+            Ok(_n) => {},
+            Err(e) => {
+                Log::error(&e.to_string());
+                Log::flush();
+                continue;
+            }
+        };
+        let cleaned = buffer.trim();
+
+        match cleaned {
+            "q" => {
+                break;
+            },
+            "w" => {
+                current_volume += volume_delta;
+                let result = handle.set_volume(
+                    Volume::Decibels(current_volume),
+                    volume_tween
+                );
+                match result {
+                    Ok(()) => {},
+                    Err(e) => {
+                        Log::error(&e.to_string());
+                        Log::flush();
+                        continue;
+                    }
+                };
+            },
+            "s" => {
+                current_volume -= volume_delta;
+                let result = handle.set_volume(
+                    Volume::Decibels(current_volume),
+                    volume_tween
+                );
+                match result {
+                    Ok(()) => {},
+                    Err(e) => {
+                        Log::error(&e.to_string());
+                        Log::flush();
+                        continue;
+                    }
+                };
+            },
+            &_ => { }
+        }
+
+        thread::sleep(tick_delta);
+    };
+
+    let result = handle.stop(tween::Tween {
+        start_time: StartTime::Immediate,
+        duration: time::Duration::from_millis(500),
+        easing: tween::Easing::Linear
+    });
+    match result {
+        Ok(()) => {},
+        Err(e) => {
+            panic!("{}", Log::fatal(&e.to_string()));
+        }
+    };
+
+    Log::flush();
+    thread::sleep(time::Duration::from_millis(1000));
+    println!("Thank you byebye!!");
+}
---
+++ white_noise_player_v1_gui/src/main.rs
@@ -0,0 +1,266 @@
+use std::{ time, thread };
+use std::sync::mpsc;
+use std::sync::mpsc::{ Sender, Receiver };
+use iced::{
+    button,
+    Sandbox, Settings,
+    Button, Element, Column, Text, Alignment, Length,
+    alignment::{ Horizontal }
+};
+use kira::{
+    tween, StartTime, Volume, LoopBehavior
+};
+use kira::manager::{
+    AudioManager, AudioManagerSettings,
+    backend::cpal::CpalBackend
+};
+use kira::sound::static_sound::{
+    StaticSoundData, StaticSoundSettings
+};
+use casual_logger::Log;
+
+enum ControlMessage {
+    Control(f64),
+    Quit
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PlayerMessage {
+    VolumeUpPressed,
+    VolumeDownPressed,
+    QuitPressed
+}
+
+struct Player {
+    current_volume: f64,
+    volume_delta: f64,
+    control_channel: Sender<ControlMessage>,
+    is_alive: bool,
+
+    volume_up_button: button::State,
+    volume_down_button: button::State,
+    quit_button: button::State
+}
+
+impl Sandbox for Player {
+    type Message = PlayerMessage;
+    
+    fn new() -> Self {
+        let (
+            control_tx,
+            control_rx
+        ): (Sender<ControlMessage>, Receiver<ControlMessage>) = mpsc::channel();
+        
+        thread::spawn(move || {
+            // sound setting
+            let result = AudioManager::<CpalBackend>::new(
+                AudioManagerSettings::default()
+            );
+            let mut manager = match result {
+                Ok(m) => m,
+                Err(e) => {
+                    panic!("{}", Log::fatal(&e.to_string()));
+                }
+            };
+            let result = StaticSoundData::from_file(
+                "noise.wav",
+                StaticSoundSettings::new().loop_behavior(
+                    LoopBehavior {
+                        start_position: 0.0
+                    }
+                )
+            );
+            let sound_data = match result {
+                Ok(d) => d,
+                Err(e) => {
+                    panic!(
+                        "{}\nYou need to put noise.wav in the same directory.",
+                        Log::fatal(&e.to_string())
+                    );
+                } 
+            };
+            
+            // start sound on sound thread
+            let result = manager.play(sound_data);
+            let mut handle = match result {
+                Ok(h) => h,
+                Err(e) => {
+                    panic!("{}", Log::fatal(&e.to_string()));
+                }
+            };
+            
+            loop {
+                let result = control_rx.recv();
+                let recv = match result {
+                    Ok(r) => r,
+                    Err(e) => {
+                        Log::error(&e.to_string());
+                        Log::flush();
+                        continue;
+                    }
+                };
+                
+                match recv {
+                    ControlMessage::Control(n) => {
+                        let result = handle.set_volume(
+                            Volume::Decibels(n),
+                            tween::Tween {
+                                start_time: StartTime::Immediate,
+                                duration: time::Duration::from_millis(250),
+                                easing: tween::Easing::Linear
+                            }
+                        );
+                        match result {
+                            Ok(()) => {},
+                            Err(e) => {
+                                Log::error(&e.to_string());
+                                Log::flush();
+                                continue;
+                            }
+                        };
+                    },
+                    ControlMessage::Quit => {
+                        let result = handle.stop(tween::Tween {
+                            start_time: StartTime::Immediate,
+                            duration: time::Duration::from_millis(500),
+                            easing: tween::Easing::Linear
+                        });
+                        match result {
+                            Ok(()) => {},
+                            Err(e) => {
+                                Log::error(&e.to_string());
+                                Log::flush();
+                                continue;
+                            }
+                        };
+                    
+                        thread::sleep(time::Duration::from_millis(500));
+                        break;
+                    },
+                };
+            };    
+        });
+
+        Self {
+            current_volume: 0.0,
+            volume_delta: 2.0,
+            control_channel: control_tx,
+            is_alive: true,
+
+            volume_up_button: button::State::default(),
+            volume_down_button: button::State::default(),
+            quit_button: button::State::default()
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("WhiteNoisePLayer")
+    }
+
+    fn view(&mut self) -> Element<PlayerMessage> {
+        Column::new()
+            .padding(20)
+            .spacing(20)
+            .align_items(Alignment::Center)
+            .push(
+                Text::new("Welcome to WhiteNoisePlayer!!")
+                .horizontal_alignment(Horizontal::Center)
+            )
+            .push(
+                Text::new("Volume")
+                .horizontal_alignment(Horizontal::Center)
+            )
+            .push(
+                Button::new(
+                    &mut self.volume_up_button,
+                    Text::new("+")
+                    .horizontal_alignment(Horizontal::Center)
+                )
+                .width(Length::Units(64))
+                .on_press(PlayerMessage::VolumeUpPressed)
+            )
+            .push(
+                Button::new(
+                    &mut self.volume_down_button,
+                    Text::new("-")
+                    .horizontal_alignment(Horizontal::Center)
+                )
+                .width(Length::Units(64))
+                .on_press(PlayerMessage::VolumeDownPressed)
+            )
+            .push(
+                Text::new("Thank you !!")
+                .horizontal_alignment(Horizontal::Center)
+            )
+            .push(
+                Button::new(
+                    &mut self.quit_button,
+                    Text::new("quit")
+                    .horizontal_alignment(Horizontal::Center)
+                )
+                .width(Length::Units(64))
+                .on_press(PlayerMessage::QuitPressed)
+            )
+        .into()
+    }
+
+    fn update(&mut self, message: PlayerMessage) {
+        match message {
+            PlayerMessage::VolumeUpPressed => {
+                self.current_volume += self.volume_delta;
+                let result = self.control_channel.send(
+                    ControlMessage::Control(self.current_volume)
+                );
+                match result {
+                    Ok(()) => {},
+                    Err(e) => {
+                        Log::error(&e.to_string());
+                        Log::flush();
+                    }
+                };
+            },
+            PlayerMessage::VolumeDownPressed => {
+                self.current_volume -= self.volume_delta;
+                let result = self.control_channel.send(
+                    ControlMessage::Control(self.current_volume)
+                );
+                match result {
+                    Ok(()) => {},
+                    Err(e) => {
+                        Log::error(&e.to_string());
+                        Log::flush();
+                    }
+                };
+            },
+            PlayerMessage::QuitPressed => {
+                let result = self.control_channel.send(ControlMessage::Quit);
+                match result {
+                    Ok(()) => {},
+                    Err(e) => {
+                        panic!("{}", Log::fatal(&e.to_string()));
+                    }
+                };
+                
+                Log::flush();
+                thread::sleep(time::Duration::from_millis(500));
+                self.is_alive = false;
+            }
+        };
+    }
+
+    fn should_exit(&self) -> bool {
+        !self.is_alive
+    }
+}
+
+pub fn main() -> iced::Result {
+    Log::remove_old_logs();
+
+    // gui config
+    let mut settings = Settings::default();
+    settings.window.size = (300, 300);
+    settings.window.resizable = false;
+
+    // gui start
+    Player::run(settings)
+}
